78d92e2ed0445677641a76615c5d264db14704c3
hvsssen7 <hvsssen7@gmail.com>
2025-04-10 21:53:07 +0100

    adjust dependencies comptability

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- Add this line
 
+import { AppComponent } from './app.component';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
